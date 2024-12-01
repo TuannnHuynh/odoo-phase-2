9c6a3daef8cb6cbdc3ca06647f7edf7aba5abded
Tuấn Huỳnh Anh <huynhanhtuan8547@gmail.com>
2024-12-01 20:05:06 +0700

    Design UI About us and Setting company

--- addons/TuanHA_helloworld/models/about_us.py
+++ addons/TuanHA_helloworld/models/about_us.py
@@ -1,6 +1,9 @@
-from odoo import fields, models, api
+from odoo import fields, models, tools, modules, api
 from odoo.exceptions import ValidationError
+from odoo.modules.module import get_module_resource
 import re
+import base64
+import os
 
 class ResCompany(models.Model):
     _inherit = "res.company"
@@ -20,63 +23,123 @@ class ResCompany(models.Model):
     tiktok_account = fields.Char(string="TikTok Account")
     zalo_account = fields.Char(string="Zalo Account")
 
-    # Trường Icon
-    twitter_icon = fields.Char(string="Twitter Icon")
-    facebook_icon = fields.Char(string="Facebook Icon")
-    github_icon = fields.Char(string="GitHub Icon")
-    linkedin_icon = fields.Char(string="LinkedIn Icon")
-    youtube_icon = fields.Char(string="YouTube Icon")
-    instagram_icon = fields.Char(string="Instagram Icon")
-    tiktok_icon = fields.Char(string="TikTok Icon")
-    zalo_icon = fields.Char(string="Zalo Icon")
+    def _get_default_image(self):
+        image_path = modules.get_module_resource('TuanHA_helloworld', 'static/img', 'twitter.png')
+        if not os.path.exists(image_path):
+            raise ValidationError(f"File không tồn tại: {image_path}")
+        with open(image_path, 'rb') as image_file:
+            return base64.b64encode(image_file.read())
+
+    # Trường Icon với ảnh mặc định
+    twitter_icon = fields.Binary(
+        string="Twitter Icon", 
+        default="_get_default_image",
+        attachment=True
+    )
+    facebook_icon = fields.Binary(
+        string="Facebook Icon", 
+        default="_get_default_image",
+        attachment=True
+    )
+    github_icon = fields.Binary(
+        string="GitHub Icon", 
+        default="_get_default_image",
+        attachment=True
+    )
+    linkedin_icon = fields.Binary(
+        string="LinkedIn Icon", 
+        default="_get_default_image",
+        attachment=True
+    )
+    youtube_icon = fields.Binary(
+        string="YouTube Icon", 
+        default="_get_default_image",
+        attachment=True
+    )
+    instagram_icon = fields.Binary(
+        string="Instagram Icon", 
+        default="_get_default_image",
+        attachment=True
+    )
+    tiktok_icon = fields.Binary(
+        string="TikTok Icon", 
+        default="_get_default_image",
+        attachment=True
+    )
+    zalo_icon = fields.Binary(
+        string="Zalo Icon", 
+        default="_get_default_image",
+        attachment=True
+    )
+
+    
+
+    # def _get_default_image(self, image_name):
+    #     """
+    #     Trả về dữ liệu base64 của hình ảnh mặc định từ static/src/img.
+    #     """
+    #     image_path = modules.get_module_resource('TuanHA_helloworld', 'static/src/img', image_name)
+    #     if image_path:
+    #         with open(image_path, 'rb') as f:
+    #             return tools.image_resize_image_big(base64.b64encode(f.read()))
+    #     return False
+
+    # def _get_default_twitter_icon(self):
+    #     return self._get_default_image('twitter.png')
+
+    # def _get_default_facebook_icon(self):
+    #     return self._get_default_image('facebook.png')
+
+    # def _get_default_github_icon(self):
+    #     return self._get_default_image('github.png')
+
+    # def _get_default_linkedin_icon(self):
+    #     return self._get_default_image('linkedin.png')
+
+    # def _get_default_youtube_icon(self):
+    #     return self._get_default_image('youtube.png')
+
+    # def _get_default_instagram_icon(self):
+    #     return self._get_default_image('instagram.png')
+
+    # def _get_default_tiktok_icon(self):
+    #     return self._get_default_image('tiktok.png')
+
+    # def _get_default_zalo_icon(self):
+    #     return self._get_default_image('zalo.png')
 
     @api.constrains(
-        'twitter_icon', 'facebook_icon', 'github_icon', 'linkedin_icon',
-        'youtube_icon', 'instagram_icon', 'tiktok_icon', 'zalo_icon'
+    'twitter_account', 'facebook_account', 'github_account', 'linkedin_account',
+    'youtube_account', 'instagram_account', 'tiktok_account', 'zalo_account'
     )
-    def _check_icon_urls(self):
+    def _check_account_urls(self):
         """
-        Kiểm tra định dạng URL hợp lệ cho các trường icon.
+        Kiểm tra định dạng URL hợp lệ cho các trường account.
         """
         url_regex = re.compile(
             r'^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-zA-Z0-9]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/.*)?$'
         )
         for record in self:
-            for field_name in [
-                'twitter_icon', 'facebook_icon', 'github_icon', 'linkedin_icon',
-                'youtube_icon', 'instagram_icon', 'tiktok_icon', 'zalo_icon'
+            for field_name, specific_checks in [
+                ('twitter_account', ['twitter.com/','https://twitter.com/']),
+                ('facebook_account', ['facebook.com/', 'https://www.facebook.com/']),
+                ('github_account', ['github.com/','https://github.com/']),
+                ('linkedin_account', ['linkedin.com/in/','https://linkedin.com/in/']),
+                ('youtube_account', ['youtube.com/@','https://www.youtube.com/@']),
+                ('instagram_account', ['instagram.com/','https://instagram.com/']),
+                ('tiktok_account', ['www.tiktok.com/@','https://www.tiktok.com/@']),
+                ('zalo_account', ['zalo.me/','https://zalo.me/']),
             ]:
                 field_value = getattr(record, field_name)
-                if field_value and not url_regex.match(field_value):
-                    raise ValidationError(f"Giá trị '{field_value}' trong trường {field_name.replace('_', ' ')} không phải là một URL hợp lệ.")
-
-    @api.onchange(
-        'twitter_icon', 'facebook_icon', 'github_icon', 'linkedin_icon',
-        'youtube_icon', 'instagram_icon', 'tiktok_icon', 'zalo_icon'
-    )
-    def _onchange_icon_requires_account(self):
-        """
-        Đặt cảnh báo khi nhập icon mà chưa nhập account tương ứng.
-        """
-        account_fields = {
-            'twitter_icon': 'twitter_account',
-            'facebook_icon': 'facebook_account',
-            'github_icon': 'github_account',
-            'linkedin_icon': 'linkedin_account',
-            'youtube_icon': 'youtube_account',
-            'instagram_icon': 'instagram_account',
-            'tiktok_icon': 'tiktok_account',
-            'zalo_icon': 'zalo_account',
-        }
-
-        for icon_field, account_field in account_fields.items():
-            icon_value = self[icon_field]
-            account_value = self[account_field]
-            if icon_value and not account_value:
-                self[icon_field] = False  # Reset lại trường icon
-                return {
-                    'warning': {
-                        'title': "Cảnh báo",
-                        'message': f"Bạn phải nhập {account_field.replace('_', ' ')} trước khi nhập {icon_field.replace('_', ' ')}."
-                    }
-                }
+                if field_value:
+                    # Kiểm tra URL hợp lệ
+                    if not url_regex.match(field_value):
+                        raise ValidationError(
+                            (f"Giá trị '{field_value}' trong trường {field_name.replace('_', ' ')} không phải là một URL hợp lệ.")
+                        )
+                    
+                    # Kiểm tra các tiền tố cụ thể
+                    if specific_checks and not any(field_value.startswith(check) for check in specific_checks):
+                        raise ValidationError(
+                            (f"Link trong trường {field_name.replace('_', ' ')} phải bắt đầu bằng một trong các giá trị sau: {', '.join(specific_checks)}.")
+                        )
